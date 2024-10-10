4d05e4306424ac60ba48eedf4fbda84d6a1889f2
midorikuma <tatsubear@gmail.com>
2024-10-10 21:47:42 +0900

    test OGPGenerator

--- OGPGenerator/src/index.ts
+++ OGPGenerator/src/index.ts
@@ -1,19 +1,26 @@
-import { handleRequest } from './requestHandler';
-import { handleImageRequest } from './imageHandler';
-
 addEventListener('fetch', (event: any) => {
-  if (event.request.method === 'OPTIONS') {
+  const request = event.request;
+  if (request.method === 'OPTIONS') {
     event.respondWith(handleOptionsRequest());
-    return;
-  }
-
-  const url = new URL(event.request.url);
-  if (url.pathname.startsWith('/images/')) {
-    event.respondWith(handleImageRequest(event.request));
   } else {
-    event.respondWith(handleRequest(event.request));
+    event.respondWith(handleRequest(request));
   }
 });
+
+async function handleRequest(request: Request) {
+    const response = await fetch(request)
+    return new HTMLRewriter()
+      .on('head', {
+        element(head) {
+          head.append(`
+            <meta property="og:title" content="特別なブログ記事のタイトル">
+            <meta property="og:description" content="特別な記事の説明">
+            <meta property="og:image" content="https://gb.huedpaw.com/wp-content/uploads/2023/05/%E7%8F%BE%E5%A0%B4%E3%82%AF%E3%83%9E-225x300.png">
+          `, { html: true })
+        }
+      })
+      .transform(response)
+}
 
 function handleOptionsRequest(): Response {
   return new Response(null, {
